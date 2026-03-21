73b5b65968521af8286fe6251170e37c7971b32c
Jessica Sun <jessicasunnz1217@gmail.com>
2026-03-21 16:13:34 +1300

    home page style

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+console.log(dirname);
+
+app.use(express.stctic(path.join(dirname, 'public')));
 
 app.use(express.static('public'));
 
 app.get('/chat', (req, res) => {
-     res.send('welcome');    
+    res.send('welcome');
 });
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send('Welcome home');
 });
 
